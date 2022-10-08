55dcb0f3f6979ecd1c8aee878a1da73bdd45033a
HikaruKawano <108588173+HikaruKawano@users.noreply.github.com>
2022-10-08 17:20:48 -0300

    add Modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import { gql, useQuery } from '@apollo/client';
+import { useState } from 'react';
+import Modal from 'react-modal';
 import Card from '../src/atoms/Card';
 import Header from './atoms/header';
 import { Colors } from './GlobalStyle';
@@ -27,7 +29,16 @@ const GET_FINANCES = gql`
 function App() {
   const { data } = useQuery<{ finances: finance[] }>(GET_FINANCES);
 
-  console.log(data);
+  const [isOpenModal, setIsOpenModal] = useState(false)
+
+  function openModal() {
+    setIsOpenModal(true);
+  }
+
+  function closeModal() {
+    setIsOpenModal(false);
+  }
+
   return (
     <>
       <Header>
@@ -53,7 +64,7 @@ function App() {
 
         <section id='transection'>
           <h2 className='sr-only'>Transações</h2>
-          <a href='#' className='button new'> + Nova tansação</a>
+          <button className='button new' onClick={() => openModal()}> + Nova tansação</button>
           <table id="data-table">
             <thead>
               <tr>
@@ -64,22 +75,85 @@ function App() {
                 <th></th>
               </tr>
             </thead>
-            {data?.finances.map(finance => 
-              
-              <tbody><tr data-index="0">
-               <td className="production" key={finance.id}>{finance.name}</td>
-                <td className="description" key={finance.id}>{finance.description}</td>
-               <td className="income" key={finance.id}>{finance.price}</td>
-               <td className="date" key={finance.id}>{finance.date}</td>
-              <td> <img src="../src/assets/minus.svg" alt="Remover transação" /></td>
+            {data?.finances.map(finance =>
 
-            </tr></tbody>
+              <tbody>
+                <tr data-index="0" key={finance.id}>
+                  <td className="production" >{finance.name}</td>
+                  <td className="description" >{finance.description}</td>
+                  <td className="income" >{finance.price}</td>
+                  <td className="date" >{finance.date}</td>
+                  <td> <img src="../src/assets/minus.svg" alt="Remover transação" /></td>
 
-              )}
-            
+                </tr>
+              </tbody>
+
+            )}
+
           </table>
+
         </section>
+
+        <Modal
+          isOpen={isOpenModal}
+          onRequestClose={closeModal}
+          overlayClassName={"modal-overlay active"}    
+          className="modal-content"
+        >
+          
+            <div className="modal">
+              <div className="form">
+                <h2>Nova transação</h2>
+                <form action="" >
+                  <div className="input-group">
+
+                    <label className="sr-only" >Produto</label>
+
+                    <input type="text" id="production" name="production" placeholder="Produto" />
+
+                  </div>
+
+
+                  <div className="input-group">
+
+                    <label className="sr-only" >Descrição</label>
+
+                    <input type="text" id="description" name="description" placeholder="Descrição" />
+
+                  </div>
+
+                  <div className="input-group">
+
+                    <label className="sr-only" >Valor</label>
+
+                    <input type="number" step="0.01" id="amount" name="amount" placeholder="0,00" />
+
+                    <small className="help">Usar o sinal - (negativo) para despesas e , (virgula) para casas desimais</small>
+                  </div>
+
+                  <div className="input-group">
+
+                    <label className="sr-only" >Descrição</label>
+
+                    <input type="date" id="date" name="date" placeholder="date" />
+                  </div>
+
+                  <div className="input-group action">
+                    <button className="button cancel" onClick={closeModal} >Cancelar</button>
+                    <button className='button confirm'>Salvar</button>
+                  </div>
+
+
+                </form>
+
+
+
+
+              </div>
+            </div>
+        </Modal>
       </main>
+
     </>
   );
 }
--- frontend/src/atoms/ModalCadastro/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-
-const PopUp_Cadastro: React.FC = () => {
-  return <div />;
-}
-
-export default PopUp_Cadastro;
